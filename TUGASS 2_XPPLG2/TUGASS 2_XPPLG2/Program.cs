6989e9d32bedb894827f06b8861d5e7c80b41e32
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 09:55:30 +0700

    Add files via upload

---
+++ TUGASS 2_XPPLG2/TUGASS 2_XPPLG2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGASS_2_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+            {
+                total += i;
+                i++;
+            }
+            Console.WriteLine("total = " + total);
+        }
+    }
+}
