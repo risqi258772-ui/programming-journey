6a410a41efd265ae49cee864ec0f48b772c9a526
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 09:29:30 +0700

    Add files via upload

---
+++ Praktik6.8_ X PPLG 2/Praktik6.8_ X PPLG 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._8__X_PPLG_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses yang Akan Diulang ---
+                Console.WriteLine("-----------------------------------");
+                Console.Write("Masukkan nama anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama} ! Proses telas selesai.");
+                //--------------------------------------
+                //Pertanyaan untuk Mengulang
+                Console.Write("Apakah anda ingin mengulang lagi? (ya/tidak): ");
+                ulangiPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris Kosong Untuk Kerapian
+
+                //Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+              Console.WriteLine("Terima kasih.Program selesai.");
+        }
+    }
+}
