fb1b69f5c72ad18f424d62bb9cdb8df93c6426c2
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 14:26:57 +0700

    Add files via upload

---
+++ Tugas_Konversi suhu/Tugas_Konversi suhu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Konversi_suhu
+{
+    internal class Program
+    {
+        // Fungsi KonversiSuhu : mengubah suhu dari Celcius dari Fahrenhiet
+        static double konversisuhu(double c)
+        {
+            double f = (c * 9 / 5) + 32;
+            return f;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== program KonversiSuhu Celcius ke Fahrenheit ===");
+
+            // Input dari pengguna
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            double suhuC = Convert.ToDouble(Console.ReadLine());
+
+            //Panggil fungsi KonversiSuhu
+            double suhuF = konversisuhu(suhuC);
+
+            //Tampilkan Hasil Konversi
+            Console.WriteLine($"{suhuC}℃ sama dengan {suhuF}℉");
+
+            Console.WriteLine("=== program selesai ===");
+        }
+    }
+}
