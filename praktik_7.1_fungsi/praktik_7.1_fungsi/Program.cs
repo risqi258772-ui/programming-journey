99d18e1107f34e8850ead7f6a766f38b1268e175
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 14:21:36 +0700

    Add files via upload

---
+++ praktik_7.1_fungsi/praktik_7.1_fungsi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_7._1_fungsi
+{
+    internal class Program
+    {
+        static void sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            sapa(); // Memanggil fungsi
+        }
+    }
+}
