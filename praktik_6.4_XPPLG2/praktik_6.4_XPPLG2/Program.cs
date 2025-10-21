c09c08b346fdabb8399d267f7dda82073ab34edc
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 09:48:24 +0700

    Add files via upload

---
+++ praktik_6.4_XPPLG2/praktik_6.4_XPPLG2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._4_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Inisialisasi variabel penghitung(counter)
+            int hitungan = 5;
+
+            Console.WriteLine("Memulai hitungan mundur:");
+            //2. Perulangan 'while'
+            // Kondisi: Selama nilai 'hitungan' lebih besar dari 0
+            while (hitungan > 0)
+            {
+                //Blok kode di dalam perulangan
+                Console.WriteLine($"Hitung: {hitung}");
+
+                //3. Update variabel penghitung (decrement/pengurangan)
+                //Ini PENTING agar perulangan tidak menjadi loop tak terbatas (infinite loop)
+                hitung = hitung - 1;
+                // atau bisa juga ditulis hitungan--;
+            }
+            // Kode setelah perulangan selesai
+            Console.WriteLine("Selesai1 Hitungan mundur telah berakhir.");
+        }
+    }
+}
