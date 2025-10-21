f5fb6d7898f2951d0be7b4e12b4ab55424eb0fc8
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 14:26:14 +0700

    Add files via upload

---
+++ Tugas_fungsi_kali/Tugas_fungsi_kali/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_fungsi_kali
+{
+    internal class Program
+    {
+        static double Kali(double x, double y, double z)
+        {
+            return x + y + z; 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka pertama: ");
+            double bil1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua: ");
+            double bil2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan angka ketiga: ");
+            double bil3 = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = Kali(bil1, bil2, bil3);
+
+            Console.WriteLine($"\nHasul dari {bil1} * {bil2} * {bil3} = {hasil}");
+        }
+    }
+}
