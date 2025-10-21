adb350c5e56b9f9d5867ba91500307ee79fcf580
risqi258772-ui <risqi258772@student.smkn1kandeman.sch.id>
2025-10-21 09:49:21 +0700

    Add files via upload

---
+++ Praktik6.5_X PPLG 2/Praktik6.5_X PPLG 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._5_X_PPLG_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisialisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine("Menampilkan angka 1 sampai 20:");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                //Tampilkan nilai variabel 'angka' saat ini
+                Console.WriteLine(angka);
+
+                //Tambahkan nilai 'angka' sebanyak 1 (Increment)
+                angka++;
+
+                // Uji kondisi: Loop berlanjut selama 'angka' kurang dari atau sama dengan
+            } while (angka <= 20);
+
+            Console.WriteLine("Selesai.");
+        }
+    }
+}
